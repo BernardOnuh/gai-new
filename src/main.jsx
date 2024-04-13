af370549a6718b3c2153bf024c5c881437c651c6
Bernard.O <114490070+BernardOnuh@users.noreply.github.com>
2024-04-13 23:32:42 +0100

    push

--- src/main.jsx
+++ src/main.jsx
@@ -4,19 +4,34 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { ThirdwebProvider } from "@thirdweb-dev/react";
-import { Mumbai } from "@thirdweb-dev/chains";
+import '@rainbow-me/rainbowkit/styles.css';
+import { getDefaultConfig, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { WagmiProvider } from 'wagmi';
+import { arbitrum, base, mainnet, optimism, polygon, zora,cronos } from 'wagmi/chains';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 
-
+const config = getDefaultConfig({
+	appName: 'RainbowKit demo',
+	projectId: 'YOUR_PROJECT_ID',
+	chains: [ polygon, cronos],
+  });
+  
+  const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-    <ThirdwebProvider activeChain={Mumbai} clientId=""> 
-		<Router>
-			<App />
-		</Router>
-    </ThirdwebProvider>
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+      <ThirdwebProvider >
+        <RainbowKitProvider>
+          <Router>
+            <App />
+          </Router>
+        </RainbowKitProvider>
+        </ThirdwebProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
   </React.StrictMode>,
 );
-
 
